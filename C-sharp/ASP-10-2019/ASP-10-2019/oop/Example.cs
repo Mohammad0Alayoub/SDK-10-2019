b81d496ca8a1b42a82246625300ab60ca7d49a63
baraa96 <34281301+baraa96@users.noreply.github.com>
2019-11-13 17:08:55 +0200

    13-11-2019

--- C-sharp/ASP-10-2019/ASP-10-2019/oop/Example.cs
+++ C-sharp/ASP-10-2019/ASP-10-2019/oop/Example.cs
@@ -9,17 +9,17 @@ namespace ASP_10_2019.oop
     abstract class calclater
     {
         public abstract void info();
-        public virtual double calculate(double FNumber,double SNumber)
+        public virtual double calculate(double FNumber, double SNumber)
         {
             return -1;
         }
     }
 
-    class Sum :calclater
+    class Sum : calclater
     {
         public override void info()
         {
-            Console.WriteLine("Pkease wait to Print the sumation");
+            Console.WriteLine("Please wait to Print the sumation");
         }
         public override double calculate(double FNumber, double SNumber)
         {
@@ -31,7 +31,7 @@ namespace ASP_10_2019.oop
     {
         public override void info()
         {
-            Console.WriteLine("Pkease wait to Print the Subtraction");
+            Console.WriteLine("Please wait to Print the Subtraction");
 
         }
         public override double calculate(double FNumber, double SNumber)
@@ -40,8 +40,56 @@ namespace ASP_10_2019.oop
         }
 
     }
+
+    class Multiply : calclater
+    {
+        public override void info()
+        {
+            Console.WriteLine("Please wait to Print the Multiplation");
+
+        }
+
+        public override double calculate(double FNumber, double SNumber)
+        {
+            return FNumber * SNumber;
+        }
+    }
+
+    class Divison : calclater
+    {
+        public override void info()
+        {
+            Console.WriteLine("Please wait to Print the Divison");
+
+        }
+
+        public override double calculate(double FNumber, double SNumber)
+        {
+            return FNumber / SNumber;
+        }
+    }
     class Example
     {
+        static void Main(string[] args)
+        {
+            calclater sum = new Sum();
+            calclater diffrance = new Diffrence();
+            calclater multipy = new Multiply();
+            calclater diviosn = new Divison();
+
+            double n1, n2;
+            Console.Write("Enter Number one : ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter Number two : ");
+            n2 = double.Parse(Console.ReadLine());
+            sum.info();
+            Console.WriteLine("The sum is : " + sum.calculate(n1,n2));
+            diffrance.info();
+            Console.WriteLine("The diffrance is : " + diffrance.calculate(n1,n2));
+
+
+        }
+
 
     }
 }
